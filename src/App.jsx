fa29a275cb7944a556a72d854c08ed9213ee4d97
TDSVictor <jose.a@ba.estudante.senai.br>
2024-04-04 20:26:04 -0300

    Primeiro deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cep, setCep] = useState('');
+  const [endereco, setEndereco] = useState(null);
+
+  const buscarCep = async () => {
+    try {
+      const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const data = await response.json();
+      setEndereco(data);
+    } catch (error) {
+      console.error('Erro ao buscar CEP:', error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="conteiner">
+      <input type="text" value={cep} onChange={(e) => setCep(e.target.value)} placeholder="Digite seu CEP" />
+      <button onClick={buscarCep}>Buscar</button>
+
+      {endereco && (
+        <div>
+          <h2>Endereço encontrado:</h2>
+          <p>CEP: <input type='text' value={endereco.cep} readOnly /></p>
+          <p>Logradouro: <input type='text' value={endereco.logradouro} readOnly /></p>
+          <p>Bairro: <input type='text' value={endereco.bairro} readOnly /></p>
+          <p>Cidade: <input type='text' value={endereco.localidade} readOnly /></p>
+          <p>Estado: <input type='text' value={endereco.uf} readOnly /></p>
+        </div>
+      )}
+    </div>
   )
 }
 
-export default App
+export default App;
