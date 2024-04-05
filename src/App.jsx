826b4f7fb69939ba02d3570206566e6a546748c8
TDSVictor <jose.a@ba.estudante.senai.br>
2024-04-04 21:24:10 -0300

    configurações de botões e api whatsapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function App() {
   const [cep, setCep] = useState('');
   const [endereco, setEndereco] = useState(null);
+  const nomeRef = useRef(null);
+  const telefoneRef = useRef(null);
+
+  const limparInputs = () => {
+    setCep('');
+    setEndereco(null);
+    nomeRef.current.value = '';
+    telefoneRef.current.value = '';
+  };
+
+  const enviarWhatsapp = () => {
+    const telefone = telefoneRef.current.value;
+    const nome = nomeRef.current.value;
+    const mensagem = `Olá ${nome}! Logradouro:${endereco.logradouro}, Bairro:${endereco.bairro}, Cidade: ${endereco.localidade}`;
+    const url = `https://api.whatsapp.com/send?phone=${telefone}&text=${encodeURIComponent(mensagem)}`;
+    window.open(url, '_blank');
+  };
 
   const buscarCep = async () => {
     try {
@@ -15,24 +32,35 @@ function App() {
   };
 
   return (
-    
-    <div className="conteiner">
-      <h1>Busque seu CEP</h1><br/>
-      <input type="text" value={cep} onChange={(e) => setCep(e.target.value)} placeholder="Digite seu CEP" />
-      <button onClick={buscarCep}>Buscar</button>
-
-      {endereco && (
-        <div>
-          <h2>Endereço encontrado:</h2>
-          <p>CEP: <input type='text' value={endereco.cep} readOnly /></p>
-          <p>Logradouro: <input type='text' value={endereco.logradouro} readOnly /></p>
-          <p>Bairro: <input type='text' value={endereco.bairro} readOnly /></p>
-          <p>Cidade: <input type='text' value={endereco.localidade} readOnly /></p>
-          <p>Estado: <input type='text' value={endereco.uf} readOnly /></p>
+    <div className="container">
+      <h1>BUSQUE SEU CEP</h1>
+      <div className="conteiner">
+        <input type="text" ref={nomeRef} placeholder="Digite seu Nome" />
+        <input type="tel" ref={telefoneRef} placeholder="Digite seu Telefone" />
+        <input type="text" value={cep} onChange={(e) => setCep(e.target.value)} placeholder="Digite seu CEP" />
+        {endereco && (
+          <div>
+            <h2>Endereço encontrado:</h2>
+            <div className="enderecos">
+              <label>Logradouro: </label>
+              <input type='text' value={endereco.logradouro} readOnly />
+              <label>Bairro: </label>
+              <input type='text' value={endereco.bairro} readOnly />
+              <label>Cidade: </label>
+              <input type='text' value={endereco.localidade} readOnly />
+              <label>Estado: </label>
+              <input type='text' value={endereco.uf} readOnly />
+            </div>
+          </div>
+        )}
+        <div className="buttons-env">
+          <button onClick={buscarCep}>Buscar</button>
+          <button onClick={limparInputs}>Limpar</button>
+          <button onClick={enviarWhatsapp}>Enviar WP</button>
         </div>
-      )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
